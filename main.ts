11474b702c098c621bf1d7dfe6b86a7ebc764a44
TimurLavrentsov <timlavr87@gmail.com>
2021-11-22 17:57:49 +0200

    update package.json

--- main.ts
+++
@@ -1,8 +0,0 @@
-import ApiCommon from './api/controller/common.controller';
-import ApiService from './api/controller/service.controller';
-
-const api = new ApiService({});
-
-(async () => {
-  console.dir(await api.canCreateChangeDataRequest(), { depth: null });
-})();
